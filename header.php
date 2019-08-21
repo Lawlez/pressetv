2792abe03d93988705ad5bc4c60ceb229747bcdc
Lawlez <dk.feger@gmail.com>
2019-08-21 15:56:35 +0200

    init
    init

---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html class="nojs html" lang="de-CH">
+ <head>
+
+  <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
+  <meta name="description" content="PresseTV AG"/>
+  <meta name="keywords" content="www.pressetv.ch, PresseTV Schweiz, NZZ, NZZ Format, Standpunkte Bilanz, BaslerZeitung, Basler Zeitung, BaZ, Mediengruppe in der Südostschweiz, gesundheit heute, www.gesundheit-heute.ch, www.srf.ch, Axel Springer Schweiz AG, BekanntMachung, SRF1, SRF info, Standpunkte, Content Park AG, CPAG, Standpunkte"/>
+  <meta name="generator" content="2018.1.0.386"/>
+  
+  <link rel="shortcut icon" href="images/pressetv-ag-favicon.ico?crc=249096532"/>
+  <title>PresseTV AG</title>
+  <!-- CSS -->
+  <link rel="stylesheet" type="text/css" href="css/site_global.css?crc=3937293988"/>
+  <link rel="stylesheet" type="text/css" href="css/index.css?crc=32642639" id="pagesheet"/>
+  <!-- IE-only CSS -->
+  <!--[if lt IE 9]>
+  <link rel="stylesheet" type="text/css" href="css/iefonts_index.css?crc=115770533"/>
+  <![endif]-->
+  <!-- Other scripts -->
+  <!-- JS includes -->
+  <script src="https://webfonts.creativecloud.com/source-sans-pro:n4,n3:all;open-sans:n4:all;montserrat:n4:all.js" type="text/javascript"></script>
+    <!--custom head HTML-->
+  <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+ </head>
+ <body>
+
+  <div class="clearfix" id="page"><!-- group -->
+   <div class="clearfix grpelem" id="phome"><!-- column -->
+    <a class="anchor_item colelem" id="home"></a>
+    <div class="browser_width colelem" id="u2664-bw">
+     <div class="gradient" id="u2664"><!-- column -->
+      <div class="clearfix" id="u2664_align_to_page">
+       <div class="clearfix colelem" id="pu6660"><!-- group -->
+        <a class="nonblock nontext shadow rounded-corners grpelem" id="u6660" href="http://www.bazonline.ch" target="_blank" title="www.bazonline.ch"><!-- simple frame --></a>
+        <a class="nonblock nontext shadow rounded-corners grpelem" id="u7497" href="http://www.dctp.tv/" target="_blank" title="www.dctp.tv"><!-- simple frame --></a>
+       </div>
+       <div class="clearfix colelem" id="pu6657"><!-- group -->
+        <a class="nonblock nontext shadow rounded-corners grpelem" id="u6657" href="http://www.nzz.ch/" target="_blank" title="www.nzz.ch"><!-- simple frame --></a>
+        <a class="nonblock nontext shadow rounded-corners grpelem" id="u6663" href="http://www.ringieraxelspringer.ch/de" target="_blank" title="www.ringieraxelspringer.ch"><!-- simple frame --></a>
+       </div>
+       <a class="nonblock nontext shadow rounded-corners colelem" id="u6666" href="http://www.somedia.ch" target="_blank" title="www.somedia.ch"><!-- simple frame --></a>
+      </div>
+     </div>
+    </div>
