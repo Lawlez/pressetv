aa0b25171a17347ebe0fd055dcb3b5e290fe7ab0
Lawlez <dk.feger@gmail.com>
2019-08-22 11:29:00 +0200

    index

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html class="nojs html" lang="de-CH">
+ <head>
+
+  <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
+  <meta name="description" content="PresseTV AG"/>
+  <meta name="keywords" content="www.pressetv.ch, PresseTV Schweiz, NZZ, NZZ Format, Standpunkte Bilanz, BaslerZeitung, Basler Zeitung, BaZ, Mediengruppe in der Südostschweiz, gesundheit heute, www.gesundheit-heute.ch, www.srf.ch, Axel Springer Schweiz AG, BekanntMachung, SRF1, SRF info, Standpunkte, Content Park AG, CPAG, Standpunkte"/>
+  <meta name="generator" content="2018.1.0.386"/>
+  
+  <link rel="shortcut icon" href="images/pressetv-ag-favicon.ico?crc=249096532"/>
+  <title>PresseTV AG</title>
+  <!-- CSS -->
+  <link rel="stylesheet" type="text/css" href="css/site_global.css?crc=3937293988"/>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" id="pagesheet"/>
+  <!-- IE-only CSS -->
+  <!--[if lt IE 9]>
+  <link rel="stylesheet" type="text/css" href="css/iefonts_index.css?crc=115770533"/>
+  <![endif]-->
+  <!-- Other scripts-->
+  <!-- JS includes -->
+  <script src="https://webfonts.creativecloud.com/source-sans-pro:n4,n3:all;open-sans:n4:all;montserrat:n4:all.js" type="text/javascript"></script>
+    <!--custom head HTML-->
+  <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+ </head>
+ <body>
+
+  <main role="main" class="headline">
+    <h1 class="cover-heading"><?php the_field( 'headline' ); ?></h1>
+    <p class="lead"><?php the_field( 'subheading' ); ?></p>
+    <p class="lead">
+      <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+    </p>
+  </main>
+
+  <main role="main" class="kontakt">
+    <h1 class="cover-heading">Kontakt</h1>
+    <p class="lead">PresseTV AG, Falkenstrasse 11, Postfach, CH-8021 Zürich
+Tel. +41 44 258 12 00    •    E-Mail: kontakt@pressetv.ch</p>
+    <p class="lead">
+      <a href="#" class="btn btn-lg btn-secondary">So finden Sie uns - Google map</a>
+    </p>
+    <p class="lead">Ombudsstelle
+Unabhängige Beschwerdeinstanz für Radio und Fernsehen
+Redaktionelle Sendungen können gemäss RTVG bei der zuständigen Ombudsstelle beanstandet werden.
+Die Ombudsstelle der Unabhängigen Beschwerdeinstanz für Radio und Fernsehen (UBI) erreichen Sie unter folgender Adresse:
+
+Dr. Oliver Sidler, Baarerstrasse 10, Postfach 7517, CH-6302 Zug
+
+E-Mail: sidler@ombudsman-rtv-priv.ch   •   web: www.ombudsman-rtv-priv.ch</p>
+  </main>
+
+
+  
